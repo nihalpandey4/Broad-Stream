3dfec336e8b6a3dd18d220818f48df24e852fd15
nihalpandey4 <rockingnihal1305@gmail.com>
2020-10-29 01:46:32 +0530

    redux-form added and basic validation using it

--- client/src/components/Streams/StreamCreate.js
+++ client/src/components/Streams/StreamCreate.js
@@ -1,9 +1,60 @@
 import React from 'react'
+import {Field,reduxForm} from "redux-form";
 
-export default function StreamCreate() {
-    return (
-        <div>
-            Create Stream
-        </div>
-    )
+class StreamCreate extends React.Component{
+
+    renderError=({touched,error})=>{
+        if(touched && error){
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            )
+        }
+    }
+
+    renderInputField=({input,label,meta})=>{
+        let forErrorHandlingClassName = `field ${meta.error&&meta.touched?"error":""}`
+        return (
+            <div className={forErrorHandlingClassName}>
+                <label>{label}</label>
+                <input {...input}  />
+                {this.renderError(meta)}
+            </div>
+            
+        );
+    }
+    
+    onSubmit =(formValues)=>{
+        console.log(formValues)
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Details for new stream</h2>
+                <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                    <Field name = "title" component ={this.renderInputField} label = "Enter Title" />
+                    <Field name ="description" component ={this.renderInputField} label = "Enter Description" />
+                    <button type="submit"  className = "ui button primary">Submit</button>
+                </form>
+            </div>
+        );
+    }
 }
+
+const validate  = (formValues)=>{
+    let error = {};
+    if(!formValues.title){
+        error.title="Title cannot be empty";
+    }
+    if(!formValues.description){
+        error.description= "Description cannot be empty";
+    }
+    return error;
+}
+
+export default reduxForm({
+    form:"streamCreate",
+    validate:validate
+})(StreamCreate);
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
-import {createStore} from  "redux"
+import {createStore,applyMiddleware,compose} from  "redux"
 
 import App from  "./components/App";
-import Reducers from "./reducers";
+import reducers from "./reducers";
 
-const store =createStore(Reducers);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__||compose
+const store =createStore(reducers,composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
     <Provider store={store}>
